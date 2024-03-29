77a7e353d2f7f8516d07d8c045517681c423dc13
wuxingsong <2774326573@qq.com>
2024-03-29 22:58:20 +0800

    1

--- c/20240217/main.c
+++ c/20240217/main.c
@@ -1,18 +1,15 @@
 /*************************************************************************
-	> File Name: main.c
-	> Author: wutong
-	> Mail: 2774326573@qq.com
-	> Created Time: 2024年02月17日 星期六 20时49分26秒
+        > File Name: main.c
+        > Author: wutong
+        > Mail: 2774326573@qq.com
+        > Created Time: 2024年02月17日 星期六 20时49分26秒
  ************************************************************************/
 
-#include<stdio.h>
-
-void main()
-{
-	int i,sum=0;
-	for(i=1; i<=100; i++)
-	{
-		sum = sum+i;
-	}
-	printf("sum=%d",sum);
+#include <stdio.h>
+void main() {
+  int i, sum = 0;
+  for (i = 1; i <= 100; i++) {
+    sum = sum + i;
+  }
+  printf("sum=%d", sum);
 }
