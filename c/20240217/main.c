97e94477ae481d06b88c7b7764b014350bf1364d
wuxingsong <2774326573@qq.com>
2024-03-30 22:38:51 +0800

    main

--- c/20240217/main.c
+++ c/20240217/main.c
@@ -6,7 +6,7 @@
  ************************************************************************/
 
 #include <stdio.h>
-void main() {
+int main(void) {
   int i, sum = 0;
   for (i = 1; i <= 100; i++) {
     sum = sum + i;
