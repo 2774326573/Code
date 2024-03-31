b1e5604625b9d09b88ca6d6bff0bab5d7f6c22ea
wuxingsong <2774326573@qq.com>
2024-03-31 11:17:41 +0800

    20240331

--- c/20240217/main.c
+++ c/20240217/main.c
@@ -6,10 +6,12 @@
  ************************************************************************/
 
 #include <stdio.h>
-void main() {
+int main(void) {
   int i, sum = 0;
   for (i = 1; i <= 100; i++) {
     sum = sum + i;
   }
   printf("sum=%d", sum);
+  return 0;
 }
+
--- c/20240321/main.c
+++ c/20240321/main.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(void)
+{
+  for (int i = 0; i < 100; i++) {
+    printf("hello\n");
+  }
+  return EXIT_SUCCESS;
+}
